2fc8b2f366d40a8c300afd20db45c8136ac8d1b5
BENYEMNA-Hamza-DIA <hamza.benyemna@gmail.com>
2025-03-19 22:40:41 +0100

    Ajout du workflow Terraform fix path terraform commands

--- run_pipeline_kafka_v2.sh
+++ run_pipeline_kafka_v2.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-bash start_kafka.sh
+sudo bash start_kafka.sh
 
 # 🔹 Création des topics
 echo "📌 [INFO] Création des topics..."
-bash create_topics.sh
+sudo bash create_topics.sh
 
 # 🔹 Création des Consumer Groups et démarrage des Consumers
 echo "👥 [INFO] Création des Consumer Groups..."
-bash create_consumers.sh
+sudo bash create_consumers.sh
 
 # 🔹 Lancer la production automatique de messages
-echo "🚀 [INFO] Lancement de la production automatique de messages... (1/2)"
-bash produce_messages_auto.sh
+echo "🚀 [INFO] Lancement de la production automatique de messages..."
+sudo bash produce_messages_auto.sh
 sleep 5
 
 # Test de failover
-echo "❗⚠️ [INFO] On simule une panne en arretant le noeud actif kafka1"
-docker-compose stop kafka1
-sleep 5
+#echo "❗⚠️ [INFO] On simule une panne en arretant le noeud actif kafka1"
+#sudo docker-compose stop kafka1
+#sleep 5
 
-echo "✅ Les consumers actifs seront basculés vers un autre noeud disponible"
-sleep 5
+#echo "✅ Les consumers actifs seront basculés vers un autre noeud disponible"
+#sleep 5
 
 # 🔹 Lancer la production automatique de messages
 echo "🚀 [INFO] Lancement de la production automatique de messages après fail over... (2/2)"
-bash produce_messages_auto.sh
+sudo bash produce_messages_auto.sh
 
 echo "Attente de la fin de consommation des messages"
 sleep 5
@@ -33,7 +33,7 @@ sleep 5
 
 # 🔹 Récupération des messages consommés
 echo "📥 [INFO] Récupération des messages consommés..."
-bash consume_messages_auto.sh
+sudo bash consume_messages_auto.sh
 
 echo "✅ [INFO] Pipeline Kafka terminé avec succès !"
 
