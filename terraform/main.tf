c6dcfd5a1ec61918ff53d106a2126c1864442e04
BENYEMNA-Hamza-DIA <hamza.benyemna@gmail.com>
2025-03-20 16:21:32 +0100

    Ajout du workflow Terraform fix path terraform commands

---
+++ terraform/main.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.google_credentials)
+  project     = var.gcp_project
+  region      = var.gcp_region
+}
+
+
+resource "google_compute_firewall" "allow_kafka_services" {
+  name    = "allow-kafka-services"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = var.allowed_ports
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+
+resource "google_compute_instance" "kafka_vm" {
+  name         = var.vm_name
+  machine_type = var.machine_type
+  zone         = var.gcp_zone
+
+  boot_disk {
+    initialize_params {
+      image = var.os_image
+      size  = var.disk_size
+    }
+  }
+
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+}
+
+
+output "instance_ip" {
+  value       = google_compute_instance.kafka_vm.network_interface[0].access_config[0].nat_ip
+  description = "Public IP of the Kafka VM"
+}
+
+
+output "ssh_connection" {
+  value       = "gcloud compute ssh ${google_compute_instance.kafka_vm.name} --zone=${var.gcp_zone}"
+  description = "Command to SSH into the Kafka VM"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,51 @@
+variable "google_credentials" {
+  description = "Path to the GCP credentials JSON file"
+  type        = string
+}
+
+variable "gcp_project" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "gcp_region" {
+  description = "GCP region where resources will be deployed"
+  type        = string
+  default     = "europe-west9"
+}
+
+variable "gcp_zone" {
+  description = "GCP zone for the VM"
+  type        = string
+  default     = "europe-west9-a"
+}
+
+variable "vm_name" {
+  description = "Name of the Kafka VM"
+  type        = string
+  default     = "kafka-instance"
+}
+
+variable "machine_type" {
+  description = "Machine type for the VM"
+  type        = string
+  default     = "e2-standard-2"
+}
+
+variable "os_image" {
+  description = "Operating system image"
+  type        = string
+  default     = "ubuntu-os-cloud/ubuntu-2004-lts"
+}
+
+variable "disk_size" {
+  description = "Disk size in GB"
+  type        = number
+  default     = 30
+}
+
+variable "allowed_ports" {
+  description = "List of allowed ports for firewall"
+  type        = list(string)
+  default     = ["8080", "9090", "3000", "9092", "9095", "9093", "9094", "2181", "9308"]
+}
